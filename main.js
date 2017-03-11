d125d919d4d49b810c1e1fb4dae3a554450c77c4
josephwilliams <josephwilli12@gmail.com>
2017-03-11 15:42:15 -0800

    added react boilerplate

--- main.js
+++ main.js
@@ -1,63 +1,36 @@
-// import function to render React UI
-import renderReactUI from './frontend/reactRootNode';
+'use strict'
 
+// Import modules
 const electron = require('electron')
-// Module to control application life.
+
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
+// Load environmental variables
+require('dotenv').load()
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+if (process.env.NODE_ENV === "development") {
+  let hotReloadServer = require('hot-reload-server')
+  let webpackConfig = require('./webpack.config.dev')
+  hotReloadServer(webpackConfig, {
+    publicPath: '/dist'
+  }).start()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// Create a variable to hold the window
+let mainWindow = null
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('ready', function() {
+
+  // creates a new browser window
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  // load the file
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  // Register window events
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  })
 })
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
