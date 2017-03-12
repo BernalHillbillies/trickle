ecf8b003e27cc83b226acab3725a93d0dc9c7e85
Ryan <Hall.Ryan.R@gmail.com>
2017-03-11 17:37:03 -0800

    terminal api promisify

--- api/terminal_api.js
+++ api/terminal_api.js
@@ -1,9 +1,13 @@
 const execFile = require('child_process').execFile;
 
-const checkVLCInstalledMac = () => {
-  return execFile('ls', [], {cwd: '/Applications'}, (error, stdout, stderr) => {
-    console.log(stdout.split('\n').includes('VLC.app'));
-  });
-};
+const checkVLCInstalledMac = () => (
+  new Promise((resolv, reject) => {
+    execFile('ls', [], {cwd: '/Applications'}, (error, stdout, stderr) => {
+      if(stdout.split('\n').includes('VLC.app')){ resolv(); }
+      else { reject(); }
+      // console.log(stdout.split('\n').includes('VLC.app'));
+    });
+  })
+);
 
-checkVLCInstalledMac();
+console.log(checkVLCInstalledMac());
