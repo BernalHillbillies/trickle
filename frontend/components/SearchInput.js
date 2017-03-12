7e847432153f92e169095e5b3ee399d50557951c
josephwilliams <josephwilli12@gmail.com>
2017-03-11 17:41:53 -0800

    search UI

--- frontend/components/SearchInput.js
+++ frontend/components/SearchInput.js
@@ -4,14 +4,34 @@ export default class SearchInput extends Component {
   constructor() {
     super();
     this.state = {
-      results: [],
+      results: [
+        'example result',
+        'another example :)',
+      ],
     };
   }
 
+  _handleInputChange(event) {
+    const value = event.target.value;
+    // do something with this value
+  }
+
   render() {
+    const { results } = this.state;
     return (
       <div className={'searchInputWrapper'}>
-        <input type={'text'} className={'searchInput'} />
+        <input
+          type={'text'}
+          className={'searchInput'}
+          onChange={() => this._handleInputChange}
+        />
+        <div className={'searchResultsWrapper'}>
+          {results.map((result, resultIndex) => {
+            <div className={'searchResultWrapper'} key={resultIndex}>
+              {result}
+            </div>
+          })}
+        </div>
       </div>
     );
   }
